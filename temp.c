41e3231123c7b9d7486de125130e6829860e5ad1
Justin Sorensen <91398241+sorenjus@users.noreply.github.com>
2023-02-07 22:04:03 -0500

    more comments

--- temp.c
+++ temp.c
@@ -44,28 +44,41 @@ int fd[20][2];
 int delay = 250;
 //Represents if the current while loop should be exited
 bool running = true;
+//keep count of which token is being checked
+  int counter = 0;
+  //char pointer used to transform token to a double
+  char *ptr;
 
+/*Function for the child processes.
+*reads the current value of the central process temp and adjusts
+*it's own temperature accordingly and writes that value to the parent
+*/
 void doChildWork()
 {
   while (running)
   {
+    //Read the current value of the central process
     read(fd[childNum][0], &ctemp, sizeof(double));
+    //if the value is equal to the stop value, exit the loop
     if (ctemp == stop)
     {
       running = false;
     }
     else
     {
+      //update the child process temperature
       childtemp = (alpha * childtemp) + ((1 - alpha) * ctemp);
-      // printf("child temp : %f\n", childtemp);
+      //Round the temperature to 3 decimal places
       childtemp = floorf(childtemp * 1000) / 1000;
-      fflush(NULL);
+      //write the current value of childtemp to the parent process
       write(fd[childNum + NUM_CHILDREN][1], &childtemp, sizeof(double));
     }
   }
+  //return the value of running to true so the child process does not exit all while loops
   running = true;
 }
 
+/*Child reads the current value inside the pipe and updates it's alpha*/
 void updateChildAlpha()
 {
   read(fd[childNum][0], &alpha, sizeof(double));
@@ -73,56 +86,64 @@ void updateChildAlpha()
   fflush(NULL);
 }
 
+/*Function to read the updated external process temperature from the user command. Returns the target process' PID*/
 int readETemp(char *token)
 {
-  int counter = 0;
-  char *ptr;
+  //the PID of the target process to update
   int targetChild;
 
-  /* walk through other tokens */
+  /* walk through tokens */
   while (token != NULL)
   {
+    //First token is the target child's PID
     if (counter == 1)
     {
-      // printf("%f", strtod(token, &ptr));
       targetChild = atoi(token);
     }
+    //Second token is the target's temperature
     else if (counter == 2)
     {
       childtemp = strtod(token, &ptr);
     }
+    //update the current token
     token = strtok(NULL, delim);
+    //update the counter
     counter++;
   }
+  //Iterate through the child list
   for (int i = 0; i < NUM_CHILDREN; i++)
   {
+    //When the target pid matches a child process pid
     if (children[i] == targetChild)
     {
+      //write to that processes pipe the update value
       write(fd[i][1], &childtemp, sizeof(double));
+      //update the value of child temp in the parent process
       tempArr[i] = childtemp;
     }
   }
   return targetChild;
 }
 
+/*Function read the updated value of the external temperature in the target process*/
 void setETemp()
 {
+  //Read the pipe from the parent process and update external temperature
   read(fd[childNum][0], &childtemp, sizeof(double));
+  
   printf("%d: Ext. temperature set to %0.2f\n", getpid(), childtemp);
   fflush(NULL);
 }
 
+/*Function that takens the current token command and retrieves the double. Returns the read value*/
 double setVal(char *token)
 {
-  int counter = 0;
-  char *ptr;
-
-  /* walk through other tokens */
+  /* walk through tokens until the token that holds the double is reached*/
   while (token != NULL)
   {
     if (counter == 1)
     {
-      // printf("%f", strtod(token, &ptr));
+      //convert the token to a double and return
       return strtod(token, &ptr);
     }
     token = strtok(NULL, delim);
